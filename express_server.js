609b18f6d2255529511940ea42c2a87b9ed2b3fe
ukflava <ukflava@gmail.com>
2022-06-13 09:35:46 -0700

    add BottStrap, links and scripts

--- express_server.js
+++ express_server.js
@@ -17,9 +17,9 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
  });
  
  app.get("/fetch", (req, res) => {
